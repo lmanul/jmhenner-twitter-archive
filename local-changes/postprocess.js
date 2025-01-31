38d3aa0842354bd6b90a6a92fe429a82231aa128
Manu Cornet <m@ma.nu>
2025-01-31 04:28:13 +0000

    Also remove some items in 'quick stats'

--- local-changes/postprocess.js
+++ local-changes/postprocess.js
@@ -10,19 +10,33 @@ const hideUnwantedNavItems = () => {
     const navBarItems = document.querySelectorAll('[role="menuitem"]');
     if (navBarItems.length === 0) {
         // Not finished loading yet. Try again soon.
-        window.setTimeout(onFinishedLoading, 10);
+        window.setTimeout(hideUnwantedNavItems, 10);
+        return;
     }
     for (let item of navBarItems) {
-        console.log(item);
-        console.log(item.getAttribute('aria-label'));
         if (navBarTitlesToHide.includes(item.getAttribute('aria-label'))) {
             item.style.display = 'none';
         }
     }
 };
 
+const hideUnwantedStats = () => {
+    // These class names won't ever change now!
+    const statItems = document.querySelectorAll('.css-1dbjc4n.r-1loqt21.r-1f720gc');
+    if (statItems.length === 0) {
+        // Not finished loading yet. Try again soon.
+        window.setTimeout(hideUnwantedStats, 10);
+        return;
+    }
+    // We only want to keep the first two.
+    for (let i = 2; i < statItems.length; i++) {
+        statItems[i].style.display = 'none';
+    }
+};
+
 const onFinishedLoading = () => {
     hideUnwantedNavItems();
+    hideUnwantedStats();
 };
 
 window.setTimeout(onFinishedLoading, 0);
